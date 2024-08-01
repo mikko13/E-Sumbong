31c9313e25c3b0afcccac663ce4ae7726ce5faac
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2024-08-01 12:00:24 +0800

    added files

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '46c76fdf772ac83d8a6d0cc954517e62e6e8f617',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '46c76fdf772ac83d8a6d0cc954517e62e6e8f617',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
